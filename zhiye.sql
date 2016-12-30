bd4d92b5112c3e7ddb8a9c21cd05fc42237b0c41
SnDragon <1803240383@qq.com>
2016-12-30 21:40:22 +0800

    sql0.1

---
+++ zhiye.sql
@@ -0,0 +1,34 @@
+drop database zhiye;
+create database zhiye;
+use zhiye;
+drop table `user`;
+#用户表
+create table `user`(
+	id int primary key auto_increment comment '用户id',
+    email varchar(255) unique not null comment '用户邮箱',
+    username varchar(20) not null comment '用户名',
+    password varchar(255) not null comment '用户密码',
+    sex int(1) not null default 1 comment '0表示女，1表示男'
+);
+#问题表
+create table `question`(
+	id int primary key auto_increment,
+    author_id int not null,
+    title varchar(255) not null comment '问题标题',
+    content varchar(500) not null comment '问题内容',
+    time datetime not null comment '提出时间',
+    foreign key (author_id) references user(id)
+    );
+#回答表
+create table `answer`(
+	id int primary key auto_increment,
+    question_id int not null comment '回答的问题id',
+    author_id int not null comment '回答者id',
+    content varchar(255) not null comment '回答内容',
+    time datetime not null comment '回答时间',
+    foreign key (question_id) references question(id),
+    foreign key (author_id) references user(id)
+	);
+#点赞表
+
+    
